e64b5c53390250e16e37e22f819c6f498ad1317f
Avraham Cohen <ak400400@gmail.com>
2017-05-11 03:36:51 +0300

    Fix

--- build.bat
+++ build.bat
@@ -26,4 +26,3 @@ rem copy /b 7zS.sfx + 7z_sfx.conf + dist.7z trynf.exe
 rem WinRAR\rar.exe a -r -sfx -z"rar_sfx.conf" dist trynf.exe
 
 del trynf_1.3.exe
-makesfx.exe -mf -fm -we -dn -un -oo dist\ trynf_1.3.exe dist\wTryNetFree.exe %LOCALAPPDATA%/Temp/Tnf\
